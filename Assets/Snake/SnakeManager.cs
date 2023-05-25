96cef476486015042499778c4e0ab8b1df0ab81b
VsPot07 <cocoacake84@gmail.com>
2023-05-26 01:47:52 +0900

    0526

--- Assets/Snake/SnakeManager.cs
+++ Assets/Snake/SnakeManager.cs
@@ -4,33 +4,34 @@ using UnityEngine;
 
 public class SnakeManager : MonoBehaviour
 {
-    private BackGround _bg;
     private Transform _snake = null;
     private List<Transform> snakes = new List<Transform>();
+    [SerializeField] private List<Material> snakeMats = new List<Material>();
 
     [SerializeField] private float _width;
     [SerializeField] private float _range;
     [SerializeField] private float _wiggleSpeed;
     [SerializeField] private float _moveSpeed;
+    public float Movespeed => _moveSpeed;
     float[] timers;
+
     private void Awake()
     {
+        MeshRenderer mat;
         _snake = GameObject.Find("Snake").transform;
         timers = new float[_snake.Find("Visual").childCount];
+
         for(int i = 0; i < _snake.Find("Visual").childCount; i++)
         {
             snakes.Add(_snake.Find("Visual").GetChild(i));
-            Debug.Log(_snake.Find("Visual").GetChild(i));
+            mat = _snake.Find("Visual").GetChild(i).GetComponent<MeshRenderer>();
+            snakeMats.Add(mat.material);
             timers[i] = 0f;
         }
     }
 
     private void Start()
     {
-        _bg = GameObject.Find("BG_1").GetComponent<BackGround>();
-        _bg.speed = _moveSpeed;
-        _bg = GameObject.Find("BG_2").GetComponent<BackGround>();
-        _bg.speed = _moveSpeed;
         StartCoroutine(Moving());
     }
 
@@ -38,12 +39,22 @@ public class SnakeManager : MonoBehaviour
     {
         for (int i = 0; i < snakes.Count; i++)
         {
-            StartCoroutine(SSSNake(snakes[i], i));
             yield return new WaitForSeconds(_range);
+            StartCoroutine(SSSSNake(snakes[i], i));
+            StartCoroutine(CCCColor(snakeMats[i], i));
         }
     }
 
-    IEnumerator SSSNake(Transform obj, int idx)
+    IEnumerator CCCColor(Material matm, int idx)
+    {
+        float a;
+        while(true)
+        {
+            //mat.color = new Color(1, 1, 1, )
+        }
+    }
+
+    IEnumerator SSSSNake(Transform obj, int idx)
     {
         while(true)
         {
